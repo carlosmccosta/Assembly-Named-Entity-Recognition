2481092833c69decca09f8ac0a518860486993b8
Carlos Costa <carloscosta.cmcc@gmail.com>
2017-01-29 21:39:44 +0000

    Improved F1 metric by 3 percent in annotated dataset after tuning CRF model parameters and features.

--- tools/train.sh
+++ tools/train.sh
@@ -10,11 +10,13 @@ properties_file=${2:-"${scriptdir}/../configs/crf.prop"}
 
 java_max_memory=${3:-2g}
 
+gazette_file=${4:-"${scriptdir}/../dataset/alternators-engines-gearboxes/dataset-A1-E1-G2.gazette.txt"}
+
 output_directory="${scriptdir}/../ner"
 input_file_basename=$(basename "${input_file}" | cut -d. -f1)
-output_file_basename=${4:-"${output_directory}/${input_file_basename}"}
+output_file_basename=${5:-"${output_directory}/${input_file_basename}"}
 
 
 echo "\n >>> Training ${input_file} using edu.stanford.nlp.ie.crf.CRFClassifier\n"
 
-java -mx${java_max_memory} -cp ${javaclasspath} edu.stanford.nlp.ie.crf.CRFClassifier -prop "${properties_file}" -trainFile "${input_file}" -serializeTo "${output_file_basename}-ner-model.ser.gz" > "${output_file_basename}-training-output.txt" 2>&1
+java -mx${java_max_memory} -cp ${javaclasspath} edu.stanford.nlp.ie.crf.CRFClassifier -prop "${properties_file}" -trainFile "${input_file}" -gazette "${gazette_file}" -serializeTo "${output_file_basename}-ner-model.ser.gz" > "${output_file_basename}-training-output.txt" 2>&1
